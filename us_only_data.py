34afb70b7808bfcacca15a5a4f029b426a666763
portis123 <123portis@googlemail.com>
2023-04-01 13:08:21 +0100

    Updated for new edge list with different format

--- us_only_data.py
+++ us_only_data.py
@@ -2,54 +2,90 @@
 """
 Created on Tue Mar  7 10:47:55 2023
 
-@author: moniq
+@author: Monique Brogan
+
+Filtering dataset for artists based in the US only
 """
 import pandas as pd
+import numpy as np
 
-df_locations = pd.read_excel('artists_cleaned_after_duplicates.xlsx')
+
+# load file with artist ids, names and countries, as derived from MusicBrainz
+df_locations = pd.read_excel('artist_id_names_country.xlsx')
 print(df_locations.head())
 print(df_locations)
 
-print(df_locations['country'].unique())
+# Sense check that country column only contains countries and there are no different spellings of United States
+countries = df_locations['country'].unique()
+countries = countries.astype(str)
+print(np.sort(countries))
+
+# can see there are some with United States spelled as United states, verify this
+print(df_locations[df_locations['country']== 'United states'])
+
+# replace these with the correct version of United States
+df_locations['country'] = df_locations['country'].replace('United states','United States')
+
+# verify that this has worked
+print(df_locations[df_locations['country']== 'United states'])
+
 
 # those artists not from the US or with no location available
 df_locations_non_us = df_locations.loc[df_locations["country"] !='United States']
 print(df_locations_non_us.head())
 
-user_ids = df_locations_non_us['artistId']
-#print(user_ids)
+# checking to make sure no anomalies
+df_locations_non_us['country'].unique()
 
-df_edges = pd.read_csv('df_edges_cleaned.csv')
-#print(df_edges)
+
+# getting the artist ids from those not from the United States
+artist_ids = df_locations_non_us['artistId']
+print(artist_ids)
+
+df_edges = pd.read_csv('df_edges_cleaned.csv', index_col=0)
+print(df_edges)
+
+df_edges = df_edges.iloc[1:]
+df_edges.head()
+
+# checking data type to ensure columns are integers, like the artist ids
+result = df_edges.dtypes
+print(result)
+
+# result returns object for both columns so need to convert them to integers
+
+df_edges = df_edges.astype({"0":"int","1":"int"})
 
 # removing the non-US artist ids from both columns in edge list
-df_edges_v1_cleaned = df_edges[~df_edges["V1"].isin(user_ids)]
-df_edges_v2_cleaned = df_edges_v1_cleaned[~df_edges_v1_cleaned["V2"].isin(user_ids)]
+df_edges_col1_cleaned = df_edges[~df_edges['0'].isin(artist_ids)]
+df_edges_col2_cleaned = df_edges_col1_cleaned[~df_edges_col1_cleaned['1'].isin(artist_ids)]
 
-df_edges_v2_cleaned.to_csv('edges_us_only.csv')
+df_edges_col2_cleaned.to_csv('edges_us_only.csv')
 
 # creating an updated artist file with the new edges file to ensure we only have artists with US location
-combined_nodes = pd.concat([df_edges_v2_cleaned['V1'], df_edges_v2_cleaned['V2']])
+combined_nodes = pd.concat([df_edges_col2_cleaned['0'], df_edges_col2_cleaned['1']])
+#len(combined_nodes.unique())
 
 df_artists_us_only = df_locations[df_locations["artistId"].isin(combined_nodes)]
 print(df_artists_us_only)
 
-# remove the duplicates from the artist list we have just created
+# remove the duplicates from the artist list we have just created 
 df_artists_us_unique = df_artists_us_only[~df_artists_us_only['artistName'].str.lower().duplicated()]
 print(df_artists_us_unique)
 
+# verify that only United States artists 
 print(df_artists_us_unique['country'].unique())
-
-
 
 # reset the index in the new dataframe
 df_artists_us_unique.reset_index(drop=True, inplace=True)
 
 print(df_artists_us_unique.head())
 
-#df_artists_us_unique.to_csv('artists_us_only.csv')
+df_artists_us_only.to_csv('artists_us_only.csv')
 
-df_missing_location = df_artists_us_unique[df_artists_us_unique['location'].isna()]
+
+# as some data has been collected on artist city or region, get only those artists for which the info is missing
+df_missing_location = df_artists_us_only[df_artists_us_only['location'].isna()]
 
 print(df_missing_location.head())
 
