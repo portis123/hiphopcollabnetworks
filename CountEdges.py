7e70c11a68dff04b6c5b71b8f60d441510f55640
portis123 <123portis@googlemail.com>
2023-04-13 16:02:19 +0100

    Amending comments in code, uploading output file

--- CountEdges.py
+++ CountEdges.py
@@ -2,25 +2,21 @@
 """
 Created on Wed Mar 22 14:03:34 2023
 
-@author: moniq
-Counting the number of edges between the same two artists. The original intent of this was 
-to look at edge weights and how these would work. This exercise showed, when checking some 
-of the most common pairings, that artists were often shown as collaborating or appearing on 
-the same master release as one of their aliases - as verified by checking the two involved 
-artist IDs (see 'Extracting Artist Aliases and Removing Collaborations between Artists and 
-            Themselves')
+Counting the number of edges between the same two artists. Getting edge weights for frequent collaborations
+
+@author: Monique Brogan
 """
 import pandas as pd
 
 # edge list derived from discogs dataset
-df_edges = pd.read_csv('discogs_edges.csv')
+df_edges = pd.read_csv('edges_w_locations_removed_multiple.csv', index_col=0)
 
-#df_edges.head()
+df_edges.head()
 
 # count the number of times the same nodes have collaborated
-df_edge_count = df_edges.groupby(["V1", "V2"]).size().reset_index(name="No times collaborated").sort_values('No times collaborated', ascending=False)
+df_edge_count = df_edges.groupby(["0", "1"]).size().reset_index(name="Weight").sort_values('Weight', ascending=False)
 
-df_edge_count.head()
+df_edge_count
 
 df_edge_count.to_csv('edge_count.csv', index = False)
 
