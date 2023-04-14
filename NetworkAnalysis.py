4afe4859e30e029588b15d67c61d44247cb7901f
portis123 <123portis@googlemail.com>
2023-04-14 15:10:58 +0100

    Adding location info, and creating top 10 function

--- NetworkAnalysis.py
+++ NetworkAnalysis.py
@@ -50,11 +50,6 @@ print(G)
 
 # G.edges.data()
 
-# edge_weights =[]
-# for edge1, edge2, weight in G.edges.data():
-#     edge_weights.append(weight)
-# print(edge_weights)
-
 
 # get information on number of nodes, number of edges
 discogs_n, discogs_e = G.order(), G.size()
@@ -66,29 +61,54 @@ r = nx.degree_pearson_correlation_coefficient(G, weight='Weight')
 
 print(r)
 
+# creating ground truth basis
+artists_communities = {}
+with open('artists_with_location_codes_simpler_for_dict.csv', 'r', encoding="utf8") as file:
+    reader = csv.reader(file)
+    for row in reader:
+        artist_id = row[1]
+        #artist_name = row[2]
+        location_code = int(row[6])
+        artists_communities[artist_id] = location_code
+        
+print(artists_communities)
+
+# artist location classifier
+def classifyArtistLocation(code):
+    location = "Not classified"
+    if code == 1:
+        location = "East Coast"
+    elif code == 2:
+        location = "Midwest"
+    elif code == 3:
+        location = "Southern"
+    elif code == 4:
+        location = "West Coast"
+    return location
+
+
+def top_10_between(graph):
+    betweenness_discogs = nx.betweenness_centrality(graph, weight='Weight')
+    top_10 = sorted(betweenness_discogs.items(), key = lambda x: x[1], reverse = True)[:10]
+    print('Discogs betweenness centrality (top 10):', top_10)
+    for identifier, betw_c in top_10:
+        print(identifier, artists[str(identifier)], ", associated location:", classifyArtistLocation(artists_communities[str(identifier)]))
+        
 # top 10 artists in terms of betweenness centrality for full network
-betweenness_discogs = nx.betweenness_centrality(G, weight='Weight')
-top_10 = sorted(betweenness_discogs.items(), key = lambda x: x[1], reverse = True)[:10]
-print('Discogs betweenness centrality (top 10):', top_10)
-for identifier, betw_c in top_10:
-    print(identifier, artists[str(identifier)])
-    
-    
+top_10_between(G)   
+
+
 # get information on connected components and find largest connected component
 connected_comps = [G.subgraph(s) for s in nx.connected_components(G)]
 print('Sizes of connected components', [len(c) for c in connected_comps])
 largest_component= connected_comps[0]
-len(largest_component)
+#len(largest_component)
 
 largest_graph = nx.subgraph(G, largest_component)
 
 # top 10 artists in terms of betweenness centrality for largest component
-between_cen_hd= nx.betweenness_centrality(largest_graph, weight='Weight')
-#print(between_cen_hd)
-top_10 = sorted(between_cen_hd.items(), key = lambda x: x[1], reverse = True)[:10]
-print('Discogs betweenness centrality (top 10) for largest component:', top_10)
-for identifier, betw_c in top_10:
-    print(identifier, artists[str(identifier)])
+top_10_between(largest_graph)   
+
 
 # get information on number of nodes, number of edges
 discogs_largest_n, discogs_largest_e = largest_graph.order(), largest_graph.size()
