e1b8e8769450e114ab95c8504d2b8fb2698f6ed3
portis123 <123portis@googlemail.com>
2023-04-14 17:21:27 +0100

    Removing metrics, making Weight lower case

--- ExaminingCommunities.py
+++ ExaminingCommunities.py
@@ -44,7 +44,7 @@ next(edge_list, None)  # get past the headers
 GT = nx.Graph()
 
 G = nx.parse_edgelist(edge_list, delimiter=',', create_using=GT,
-                      nodetype=int, data=(('Weight', int),))
+                      nodetype=int, data=(('weight', int),))
 
 print(G)
 
@@ -58,50 +58,56 @@ print('Sizes of the connected components', [len(c) for c in connected_comps])
 largest_component= connected_comps[0]
 
 
-# list of degrees in largest component
-degree_list = largest_component.degree()
-print(degree_list)
+# # list of degrees in largest component
+# degree_list = largest_component.degree()
+# print(degree_list)
 
-# list of nodes in largest component
-node_list = largest_component.nodes()
-print(node_list)
+# # list of nodes in largest component
+# node_list = largest_component.nodes()
+# print(node_list)
 
-# getting nodes with degree of greater than 1    
-higher_degrees = []
-for node in node_list:
-    print(node, degree_list[node])
-    if degree_list[node] > 1:
-        higher_degrees.append(node)
+# # getting nodes with degree of greater than 1    
+# higher_degrees = []
+# for node in node_list:
+#     print(node, degree_list[node])
+#     if degree_list[node] > 1:
+#         higher_degrees.append(node)
 
-print(higher_degrees)
+# print(higher_degrees)
 
-#largest_component_graph = nx.subgraph(G, largest_component)
+# #largest_component_graph = nx.subgraph(G, largest_component)
 
 
-# creating a subgraph with only those nodes in the largest component, with degrees of more than 1
-HDG = nx.subgraph(G, higher_degrees)
-HDG.number_of_nodes()
+# # creating a subgraph with only those nodes in the largest component, with degrees of more than 1
+# HDG = nx.subgraph(G, higher_degrees)
+# HDG.number_of_nodes()
 
-#largest = nx.subgraph(G, largest_component)
+largest_graph = nx.subgraph(G, largest_component)
 
 
 #len(community_louvain.best_partition(HDG))
 
 import community as community_louvain
+import community
 
 # compute the best partition
-partition = community_louvain.best_partition(HDG)
-#partition = community_louvain.best_partition(largest)
+#partition = community_louvain.best_partition(HDG)
+resolutions = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0]
+for res in resolutions:
+    partition = community_louvain.best_partition(largest_graph, weight='weight', resolution=res)
+    print("Resolution value:", res, "# found communities smaller:", max(partition.values()))
 
 
+partition = community_louvain.best_partition(largest_graph, weight='weight')
+modularity = community.modularity(partition, largest_graph, weight='weight')
+print("The modularity for the louvain algorithm when run on the largest component is", modularity)
 
 print("# found communities smaller:", max(partition.values()))
 
-density = nx.density(HDG)
-print("Network density:", density)
+#density = nx.density(HDG)
+#print("Network density:", density)
 
 
-print(partition)
 
 
 # counting number of artists in each community
@@ -118,7 +124,7 @@ for i in range(0, num_communities):
 for key, value in partition.items():
     communities_dict.setdefault(value).extend([key])
 
-print(communities_dict[17])
+print(communities_dict[0])
 
 # getting the lengths for each comumunity to be able to sort them for the largest communities
 community_lengths = {}
@@ -149,7 +155,9 @@ def top_five_communities(top_5_list, graph, communities_dictionary):
         print("largest location as proportion of total size of community:", max(location)/5)
     
 
-top_five_communities(top_5, HDG, communities_dict)
+top_five_communities(top_5, largest_graph, communities_dict)
+
+
 
 # getting all members by degree centrality of top 5 communities, and checking locations
 def top_five_communities_all(top_5_list, graph, communities_dictionary):
@@ -166,38 +174,51 @@ def top_five_communities_all(top_5_list, graph, communities_dictionary):
         print("largest location as proportion of total size of community:", max(location)/community_lengths[comm])
     
 
-top_five_communities_all(top_5, HDG, communities_dict)
+top_five_communities_all(top_5, largest_graph, communities_dict)
 
 
-# evaluating results
+####################################################################################################
+# top 5 communities by number of members
+comms_sorted = sorted(community_lengths.items(), key = lambda x: x[1], reverse = True)
+print(comms_sorted)
+top_5 = sorted(community_lengths.items(), key = lambda x: x[1], reverse = True)[:5]
+print(top_5)
+
+# getting top 5 members by weighted degree of top 5 communities, and checking locations
+def top_five_communities(top_5_list, graph, communities_dictionary):
+    for comm, count in top_5_list:
+        location = [0,0,0,0]
+        sub_community = nx.subgraph(graph, communities_dictionary[comm])
+        degree = list(sub_community.degree(weight = 'weight'))
+        #print("DEGREE:", degree)
+        srted = sorted(degree, key=lambda a: a[1], reverse=True)[:5]
+        #print("SORTED DEGREES:", srted)
+        #highest_degree_centrality = sorted(degree.items(), key = lambda x: x[1], reverse = True)[:5]
+        #print('Degree centrality: ', highest_degree_centrality)
+        for identifier, weighted_degree in srted:
+            print(comm, artists[str(identifier)], ", associated location", artists_communities[str(identifier)])
+            location[artists_communities[str(identifier)]-1]+=1
+        print(location)
+        print("largest location as proportion of total size of community:", max(location)/5)
+    
+
+top_five_communities(top_5, largest_graph, communities_dict)
 
 
-# https://scikit-learn.org/stable/modules/classes.html#module-sklearn.metrics
 
-from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_score, precision_recall_fscore_support
+# getting all members by degree centrality of top 5 communities, and checking locations
+def top_five_communities_all(top_5_list, graph, communities_dictionary):
+    for comm, count in top_5_list:
+        location = [0,0,0,0]
+        sub_community = nx.subgraph(graph, communities_dictionary[comm])
+        degree = list(sub_community.degree(weight = 'weight'))
+        srted = sorted(degree, key=lambda a: a[1], reverse=True)
+        #print('Degree centrality: ', highest_degree_centrality)
+        for identifier, weighted_degree in srted:
+            print(comm, artists[str(identifier)], ", associated location", artists_communities[str(identifier)])
+            location[artists_communities[str(identifier)]-1]+=1
+        print(location)
+        print("largest location as proportion of total size of community:", max(location)/community_lengths[comm])
+    
 
-ground_truth = [artists_communities[str(node)] for node in HDG.nodes()]
-#print(ground_truth)
-predicted = [partition[node] for node in HDG.nodes()]
-#print(predicted)
-
-c_matrix = confusion_matrix(ground_truth, predicted)
-print(c_matrix)
-
-prec_score = precision_score(ground_truth, predicted, average = 'weighted')
-print(prec_score)
-#rec_score = recall_score(ground_truth, predicted, average = 'weighted')
-#print(rec_score)
-f1 = f1_score(ground_truth, predicted, average='weighted')
-print(f1)
-
-print(precision_recall_fscore_support(ground_truth, predicted, average='weighted'))
-
-# Calculating modularity 
-mod=community.modularity(partition,HDG)
-print("Modularity: ", mod)
-
-from sklearn import metrics
-
-
-metrics.adjusted_mutual_info_score(ground_truth, predicted)  
+top_five_communities_all(top_5, largest_graph, communities_dict)
