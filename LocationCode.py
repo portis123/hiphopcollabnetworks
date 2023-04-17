998d1bc3cc221d1d41ddeb4f1c4f66ca76f0d074
portis123 <123portis@googlemail.com>
2023-04-17 20:19:07 +0100

    Tidying up code

--- LocationCode.py
+++ LocationCode.py
@@ -1,18 +1,14 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Mar 30 19:46:17 2023
-
 Adding location classifiers, the basis for ground truth, to artist details
 
 @author: Monique Brogan
-
 """
-
 import pandas as pd
 
 df_artists = pd.read_csv('artists_w_locations_removed_multiple.csv', index_col= 0)
-print(df_artists['Code'])
 
+# make Code column blank so can add location classifier in there
 df_artists = df_artists.assign(Code='')
 df_artists['Code']
 
@@ -26,20 +22,17 @@ def classifyArtistLocation(df_artists):
             df_artists.at[ind, 'Code'] = 3
         if "West Coast" in val:
             df_artists.at[ind, 'Code'] = 4
-        #df_artists.loc[ind, 'Code'] = pd.Series(listOfCodes)
-        #print(df_artists.loc[ind, 'Code'])
     return
         
 classifyArtistLocation(df_artists)
 
-
-#list(df_artists.columns)
 print(df_artists['Code'])
 
+print(df_artists['Code'].value_counts())
 
 df_artists.to_csv('artists_with_location_codes_simpler.csv')
 
-print(df_artists['Code'].value_counts())
+
 
 
     
