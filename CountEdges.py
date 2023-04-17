853cff02544899403b8ede0c0097aa5d0a0c3101
portis123 <123portis@googlemail.com>
2023-04-17 20:39:30 +0100

    Changing weight to lower case, tidying code

--- CountEdges.py
+++ CountEdges.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Mar 22 14:03:34 2023
-
 Counting the number of edges between the same two artists. Getting edge weights for frequent collaborations
 
 @author: Monique Brogan
@@ -11,15 +9,13 @@ import pandas as pd
 # edge list derived from discogs dataset
 df_edges = pd.read_csv('edges_w_locations_removed_multiple.csv', index_col=0)
 
-df_edges.head()
-
 # count the number of times the same nodes have collaborated
-df_edge_count = df_edges.groupby(["0", "1"]).size().reset_index(name="Weight").sort_values('Weight', ascending=False)
+df_edge_count = df_edges.groupby(["0", "1"]).size().reset_index(name="weight").sort_values('weight', ascending=False)
 
-df_edge_count
+# number of pairs of nodes for each number of collaborations / weight
+df_edge_count['weight'].value_counts()
 
 df_edge_count.to_csv('edge_count.csv', index = False)
 
-# number of pairs of nodes for each number of collaborations 
-df_edge_count['No times collaborated'].value_counts()
+
 
