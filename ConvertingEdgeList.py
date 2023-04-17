61866e47a0a0bb81d5e8ae21c8c48d70fe4dc969
portis123 <123portis@googlemail.com>
2023-04-17 10:55:08 +0100

    Removing print statements

--- ConvertingEdgeList.py
+++ ConvertingEdgeList.py
@@ -17,23 +17,19 @@ df_artists = pd.read_csv('artists.csv')
 
 # combine edge lists from both columns in edge list file
 combined_nodes = pd.concat([df_edges['V1'], df_edges['V2']])
-combined_nodes
 
 # get only the artists that are in the list of nodes from our edge list
 df_artists_hh_new = df_artists[df_artists["artistId"].isin(combined_nodes)]
 
 # remove the duplicates from the artist list we have just created
 df_artists_cleaned = df_artists_hh_new[~df_artists_hh_new['artistName'].str.lower().duplicated()]
-print(df_artists_cleaned)
 
 # reset the index in the new dataframe
 df_artists_cleaned.reset_index(drop=True, inplace=True)
-df_artists_cleaned
 
 # sort the resulting artist list by artist ID
 df_artists_sorted = df_artists_cleaned.sort_values(by=['artistId'])
 df_artists_sorted.reset_index(drop=True, inplace=True)
-df_artists_sorted
 
 # save list of relevant artist ids and names
 df_artists_sorted.to_csv('df_artists_cleaned.csv')
@@ -44,12 +40,8 @@ map_dict = dict(zip(df_artists_cleaned.artistId,df_artists_cleaned.artistName))
 df_edges['Names_lists1'] =  df_edges['V1'].explode().map(map_dict)
 df_edges['Names_lists2'] =  df_edges['V2'].explode().map(map_dict)
 
-#df_edges.head()
-
 # remove the artist ids and leave only the names
 edge_list_names = df_edges.filter(['Names_lists1','Names_lists2'], axis=1)
-
-edge_list_names.head()
 
 # save edge list of artist names
 edge_list_names.to_csv('df_edge_list_names.csv')
